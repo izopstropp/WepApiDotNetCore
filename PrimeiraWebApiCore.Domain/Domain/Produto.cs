a2ab5c29060cc73771720f4a4bc77694b46651db
Felipe Francisco da Silva <felipe.silva@kurier.com.br>
2019-07-02 15:27:08 -0300

    first

---
+++ PrimeiraWebApiCore.Domain/Domain/Produto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeiraWebApiCore.Domain.Domain
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ PrimeiraWebApiCore.Infrastructure.Data/Contexto/Interface/IDbConnectionFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeiraWebApiCore.Infrastructure.Data.Contexto.Interface
+{
+    public interface IDbConnectionFactory
+    {
+
+    }
+}
