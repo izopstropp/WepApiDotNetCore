2c3648ebcb656fce8b4cd3fb2387e20b22eee332
Felipe Francisco da Silva <felipe.silva@kurier.com.br>
2019-07-02 15:42:13 -0300

    adicionado webapi core

---
+++ PrimeiraWebApiCore.Infrastructure.Data/Contexto/DbConnectionFactory.cs
@@ -0,0 +1,16 @@
+﻿using PrimeiraWebApiCore.Infrastructure.Data.Contexto.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace PrimeiraWebApiCore.Infrastructure.Data.Contexto
+{
+    public class DbConnectionFactory : IDbConnectionFactory
+    {
+        public IDbConnection CreateConnection(string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PrimeiraWebApiCore.Infrastructure.Data/Contexto/Interface/IDbConnectionFactory.cs
+++ PrimeiraWebApiCore.Infrastructure.Data/Contexto/Interface/IDbConnectionFactory.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace PrimeiraWebApiCore.Infrastructure.Data.Contexto.Interface
 {
     public interface IDbConnectionFactory
     {
-
+        IDbConnection CreateConnection(string connectionString);
     }
 }
